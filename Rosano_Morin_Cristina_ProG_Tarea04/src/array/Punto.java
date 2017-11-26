b8b73c6fb638f792190d0270a5675257ee0ed2f8
crosanom <crosanom@192.168.1.43>
2017-11-26 18:34:14 +0100

    Commit TAREA 3 : Crear un constructor con parametros

--- Rosano_Morin_Cristina_ProG_Tarea04/src/array/Punto.java
+++ Rosano_Morin_Cristina_ProG_Tarea04/src/array/Punto.java
@@ -1,19 +1,27 @@
 package array;
 
 /**
- * @author crosanom
- *version1.0
+ * @author crosanom version1.0
  */
 public class Punto {
 	// Creamos los atributos privados proteger el acceso a los mismos
 
 	/**
-	 * atributos de la clase 
+	 * atributos de la clase
 	 */
 	private double x;
 	private double y;
 
+	// Este es el contructor por defecto
 	public Punto() {
+	}
+
+	// constructor con parametros de x e y 
+	
+	public Punto(double x, double y) {
+		this.x = 2.3; // pasar el valor 2.3 ojo todos los objetos que se deriven
+						// de este constructor
+		this.y = 1.3;
 
 	}
 
