0632a65335a5bbd61e3d74ed8cba22ce3f293b06
crosanom <crosanom@192.168.1.43>
2017-11-26 18:10:04 +0100

    Commit TAREA 3: Creado el paquete array dentro del proyecto Creado una clase Punto con dos atributos privados, se deja el constructor que tenemos por defecto.

---
+++ Rosano_Morin_Cristina_ProG_Tarea04/src/array/Punto.java
@@ -0,0 +1,20 @@
+package array;
+
+/**
+ * @author crosanom
+ *version1.0
+ */
+public class Punto {
+	// Creamos los atributos privados proteger el acceso a los mismos
+
+	/**
+	 * atributos de la clase 
+	 */
+	private double x;
+	private double y;
+
+	public Punto() {
+
+	}
+
+}
